35a1598c8cddbfdcb6f46a59abb2f15301946577
Prashanth <you@example.com>
2021-06-10 12:49:47 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './Component/header'
+import AllCountries_list from './Component/all_Countries_list'
+import Country from './Component/Coutry'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header></Header>
+
+      <Switch>
+
+        <Route exact path="/">
+          <AllCountries_list />
+        </Route>
+
+        <Route  path="/countries/:name" component={Country} >
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
